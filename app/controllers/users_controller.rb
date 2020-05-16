501748bcc9be18a6be8c641e6104ce7861edf104
SrinivasaSaran <121004244@sastra.ac.in>
2020-05-16 23:00:22 +0530

    Fully working User sign up page with bcrypt password encryption, activerecord validations and flash messages are done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
   def index
   end
+
+  def create
+    if !(params[:password_digest] == params[:password_confirmation_digest])
+      redirect_to new_user_path, error: "Password Mismatch.\n You must enter same content in both password fields"
+    else
+      user = User.create(name: params[:name],
+                         role: params[:role],
+                         password: params[:password_digest],
+                         email: params[:email],
+                         password_confirmation: params[:password_confirmation_digest],
+                         address: params[:address],
+                         phone_no: params[:phone_no])
+      if user.valid?
+        session[:current_user_id] = user.id
+        redirect_to menus_path
+      else
+        flash[:invalid] = user.errors.full_messages.join(". ")
+        redirect_to new_user_path
+      end
+    end
+  end
+
+  def new
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_many :orders
+  has_secure_password
+  validates :name, :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :address, :phone_no, presence: true
 end
