546d660be481f47c42b0357224bb9162cef69af2
SrinivasaSaran <121004244@sastra.ac.in>
2020-05-24 14:46:33 +0530

    Now admin can add a new Menu with 2 to 50 characters. It's done with validations. Also, admin can't make an empty menu active. Both are aided with flash messages.

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -4,13 +4,26 @@ class MenusController < ApplicationController
   def index
   end
 
+  def create
+    menu = Menu.create(name: params[:menu_name], active_status: false)
+    if menu.valid?
+      redirect_to manage_menus_path
+    else
+      redirect_to manage_menus_path, error: menu.errors.full_messages.join(". ")
+    end
+  end
+
   def show
     @menu = Menu.find(params[:id])
     @order_id = session[:current_order_id]
   end
 
   def update
-    Menu.find(params[:id]).update!(active_status: !Menu.find(params[:id]).active_status)
-    redirect_to manage_menus_path
+    if Menu.find(params[:id]).menu_items.empty?
+      redirect_to manage_menus_path, invalid: "You can't chose an Empty Menu..!  Add Items First"
+    else
+      Menu.find(params[:id]).update!(active_status: !Menu.find(params[:id]).active_status)
+      redirect_to manage_menus_path
+    end
   end
 end
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,5 +1,6 @@
 class Menu < ApplicationRecord
   has_many :menu_items
+  validates :name, length: { in: 2..50 }, uniqueness: { case_sensitive: false }
 
   def self.active
     where(active_status: true)
