9bae468ce1985498499c0604457f3b6b1eb4e1a9
SrinivasaSaran <121004244@sastra.ac.in>
2020-05-26 23:54:04 +0530

    MenuItem can now be added by Admin. It's aided with ActiveRecord Validations

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,8 +1,22 @@
 class MenuItemsController < ApplicationController
   before_action :ensure_admin
 
+  def create
+    item = MenuItem.create(
+      name: params[:name],
+      description: params[:description],
+      menu_id: params[:menu_id],
+      price: params[:price],
+    )
+    if item.valid?
+      redirect_to edit_menu_path(params[:menu_id])
+    else
+      redirect_to edit_menu_path(params[:menu_id]), error: item.errors.full_messages.join(". ")
+    end
+  end
+
   def destroy
     MenuItem.find(params[:id]).destroy
-    redirect_to edit_menu_path
+    redirect_to edit_menu_path(params[:menu_id])
   end
 end
--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,3 +1,6 @@
 class MenuItem < ApplicationRecord
   belongs_to :menu
+  validates :name, length: { minimum: 3 }, uniqueness: { case_sensitive: false }
+  validates :description, length: { minimum: 3 }
+  validates :price, presence: true
 end
