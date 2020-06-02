427174b81aafa6ee1d0db5d4f871a4dc39a289c8
SrinivasaSaran <121004244@sastra.ac.in>
2020-06-03 03:18:13 +0530

    Invoice Overview can now been seen by admin for any date range and for any particular user

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -10,6 +10,20 @@ class AdminsController < ApplicationController
   end
 
   def stats
+    @invoices = Order.where("placed_at >= ? and placed_at <= ?", DateTime.now.beginning_of_day, DateTime.now.end_of_day)
+    if params[:from_date].to_s.length > 0 && params[:to_date].to_s.length > 0
+      @invoices = Order.where("placed_at >= ? and placed_at <= ?", params[:from_date].to_time.beginning_of_day, params[:to_date].to_time.end_of_day)
+      if (params[:username].to_s.strip.length > 0 && params[:username] != "All")
+        user_id = User.find_by(name: params[:username]).id
+        @invoices = @invoices.where(user_id: user_id)
+      end
+    end
+    @from_date = Date.today
+    @to_date = Date.today
+    @username = "All"
+    @from_date = params[:from_date] if params[:from_date].to_s.length > 0
+    @to_date = params[:to_date] if params[:to_date].to_s.length > 0
+    @username = params[:username] if (params[:username].to_s.strip.length > 0 && params[:username] != "All")
   end
 
   def role_change
